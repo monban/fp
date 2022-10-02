43a24fb16cd17f2e5739b274b7b8c60f3a2adbe0
Ian Flynn <himitsu.monban@gmail.com>
2022-10-01 22:30:54 -0400

    initial commit, with map

---
+++ map.go
@@ -0,0 +1,9 @@
+package fp
+
+func Map[C any](a []C, procedure func(C) C) []C {
+	b := make([]C, len(a))
+	for i, v := range a {
+		b[i] = procedure(v)
+	}
+	return b
+}
---
+++ test/fp_test.go
@@ -0,0 +1,95 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/matryer/is"
+	"github.com/monban/fp"
+)
+
+// Test case struct
+type tc_int struct {
+	i []int
+	e []int
+}
+
+func TestMapReturnsArraySameLength(t *testing.T) {
+	tests := [][]int{
+		{},
+		{1},
+		{1, 2},
+		{0, 1, 2},
+		{1, 2, 3, 4},
+		{1, 2, 3, 4, 5},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("case #%d", i), func(t *testing.T) {
+			is := is.New(t)
+			out := fp.Map(test, doNothing)
+			is.Equal(len(out), len(test))
+		})
+	}
+}
+
+func TestMapCallsProcedureForEachElement(t *testing.T) {
+	foo := [][]int{
+		{},
+		{0},
+		{1, 2, 3, 4, 5, 6},
+	}
+	for c, testData := range foo {
+		t.Run(fmt.Sprintf("case #%d", c), func(t *testing.T) {
+			is := is.New(t)
+			var i int
+			fp.Map(testData, func(j int) int {
+				i++
+				return 0
+			})
+			is.Equal(i, len(testData))
+		})
+	}
+}
+
+func TestMapAddOne(t *testing.T) {
+	tests := []tc_int{
+		{[]int{1, 2, 3, 4}, []int{2, 3, 4, 5}},
+		{[]int{-1, -2, -3, -4}, []int{0, -1, -2, -3}},
+		{[]int{100, 2000, 30000, 400000}, []int{101, 2001, 30001, 400001}},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("case #%d", i), func(t *testing.T) {
+			is := is.New(t)
+			out := fp.Map(test.i, addOne)
+			is.Equal(out, test.e)
+		})
+	}
+}
+
+func TestMapReverseString(t *testing.T) {
+	test := []string{"hannah", "moon", "apple", "cotton"}
+	expected := []string{"hannah", "noom", "elppa", "nottoc"}
+
+	is := is.New(t)
+
+	out := fp.Map(test, reverseString)
+	is.Equal(out, expected)
+}
+
+func doNothing(i int) int {
+	return i
+}
+
+func addOne(i int) int {
+	return i + 1
+}
+
+func reverseString(a string) string {
+	var b string
+	for _, c := range a {
+		b = string(c) + b
+	}
+	return b
+}
